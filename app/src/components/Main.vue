32b6d13f4a0d8bb35379029fb4689adebc6cdb31
m-yahya <yahya_gis@hotmail.com>
2019-10-21 20:01:38 +0200

    Add method to send transaction

--- app/src/components/Main.vue
+++ app/src/components/Main.vue
@@ -1,12 +1,47 @@
 <template>
   <div class="main">
     <h1>Bend Make Change - Hackathon 2019</h1>
+    <button @click="storeValue">submit</button>
   </div>
 </template>
 
 <script>
+import web3 from "../assets/js/web3";
+import ContractInstance from "../assets/js/ContractInstance";
 export default {
-  name: "Main"
+  name: "Main",
+  data() {
+    return {
+      account: "0x0000000000000000000000000000000000000000"
+    };
+  },
+
+  methods: {
+    async getMetamaskAccount() {
+      web3.eth.getAccounts((err, res) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        this.account = res[0];
+      });
+    },
+
+    async storeValue() {
+      // create contract instance
+      const SimpleStorage = await ContractInstance();
+      // call contract method
+      SimpleStorage.methods
+        .set(123)
+        .send({ from: this.account })
+        .then(function(receipt) {
+          console.log(receipt);
+        });
+    }
+  },
+  created() {
+    this.getMetamaskAccount();
+  }
 };
 </script>
 
