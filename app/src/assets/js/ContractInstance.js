dd95a1ea403559cc2905dccc99f3a9cbbdf15df5
m-yahya <yahya_gis@hotmail.com>
2019-10-21 19:59:15 +0200

    Add method to create contract instance

---
+++ app/src/assets/js/ContractInstance.js
@@ -0,0 +1,20 @@
+import web3 from './web3';
+import simpleStorageArtifact from "../../../../build/contracts/SimpleStorage.json";
+
+const ContractInstance = async function test() {
+
+    //This method find the network id to retrieve the configuration from truffle-config.js file
+    const networkId = await web3.eth.net.getId();
+    // Retrieve the Network configuration from truffle-config.js file
+    const deployedNetwork = simpleStorageArtifact.networks[networkId];
+    // Initializing the contract
+    const SimpleStorage = new web3.eth.Contract(
+        simpleStorageArtifact.abi,
+        deployedNetwork.address
+    );
+
+    return SimpleStorage;
+}
+
+
+export default ContractInstance;
