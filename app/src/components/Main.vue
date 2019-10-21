ce1c49b3028176a18c8bec6c81c71f26cab80a90
m-yahya <yahya_gis@hotmail.com>
2019-10-21 11:30:51 +0200

    Create vue app

---
+++ app/src/components/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="main">
+    <h1>Bend Make Change - Hackathon 2019</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Main"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
