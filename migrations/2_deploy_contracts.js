452d9dfe1630b4fca284e8dbfff72963ab209a7c
m-yahya <yahya_gis@hotmail.com>
2019-10-23 22:53:15 +0200

    Add migration to deploy Crowdsale contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
 const SimpleStorage = artifacts.require("SimpleStorage");
+const WPPCrowdsale = artifacts.require("WPPCrowdsale");
 
 module.exports = function (deployer) {
+
     deployer.deploy(SimpleStorage);
+    deployer.deploy(WPPCrowdsale, 1571871202, 1572041242, 10, 10);
 };
