ecdc155614f7d1959f954f9110e4f5115121683f
m-yahya <yahya_gis@hotmail.com>
2019-10-24 10:25:47 +0200

    Update APPCrowdsale constructor parameters

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,7 +2,9 @@ const SimpleStorage = artifacts.require("SimpleStorage");
 const WPPCrowdsale = artifacts.require("WPPCrowdsale");
 
 module.exports = function (deployer) {
+    const currentTime = Math.round((new Date()).getTime() / 1000);
+    const endTime = currentTime + 432000;
 
     deployer.deploy(SimpleStorage);
-    deployer.deploy(WPPCrowdsale, 1571871202, 1572041242, 10, 10);
+    deployer.deploy(WPPCrowdsale, currentTime, endTime, 10, 10);
 };
