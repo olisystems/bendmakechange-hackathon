a3d38e72551b1990c0bc277e2a67db9c77daf60a
m-yahya <yahya_gis@hotmail.com>
2019-10-24 14:34:15 +0200

    Update test for event detection

--- test/TestCrowdsale.js
+++ test/TestCrowdsale.js
@@ -1,4 +1,5 @@
 var WPPCrowdsale = artifacts.require('../app/src/assets/js/contracts/WPPCrowdsale')
+const truffleAssert = require('truffle-assertions');
 
 contract('WPPCrowdsale', function (accounts) {
 
@@ -8,11 +9,15 @@ contract('WPPCrowdsale', function (accounts) {
     it("Testing smart contract function invest() that allows an investor to invest funds", async () => {
         const wPPCrowdsale = await WPPCrowdsale.deployed()
 
-        await wPPCrowdsale.invest({ from: ownerID, value: 1 })
+        const result = await wPPCrowdsale.invest({ from: ownerID, value: 100000000000000000 })
+        console.log(result.logs);
+
         let investment = await wPPCrowdsale.getTotalInvestment.call()
         investment = web3.utils.fromWei(investment, "ether");
 
-        assert.equal(investment, 0.000000000000000001, 'Invalid event emitted')
+        assert.equal(investment, 0.1, 'Invalid event emitted')
+        //truffleAssert.eventEmitted(result, 'LogInvestment');
+        truffleAssert.eventEmitted(result, 'Transfer');
     })
 
 
