6f3cbf9e5266185ebc087ef1167a1a76f7d2b271
m-yahya <yahya_gis@hotmail.com>
2019-10-28 16:27:24 +0100

    Update contract deploy method

--- app/src/assets/js/ContractInstance.js
+++ app/src/assets/js/ContractInstance.js
@@ -1,19 +1,19 @@
 import web3 from './web3';
-import simpleStorageArtifact from "./contracts/SimpleStorage";
+import energySupplyArtifact from "./contracts/EnergySupply";
 
 const ContractInstance = async function test() {
 
     //This method find the network id to retrieve the configuration from truffle-config.js file
     const networkId = await web3.eth.net.getId();
     // Retrieve the Network configuration from truffle-config.js file
-    const deployedNetwork = simpleStorageArtifact.networks[networkId];
+    const deployedNetwork = energySupplyArtifact.networks[networkId];
     // Initializing the contract
-    const SimpleStorage = new web3.eth.Contract(
-        simpleStorageArtifact.abi,
+    const EnergySupply = new web3.eth.Contract(
+        energySupplyArtifact.abi,
         deployedNetwork.address
     );
 
-    return SimpleStorage;
+    return EnergySupply;
 }
 
 
