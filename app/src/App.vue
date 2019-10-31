be4616c2158c524bc37f39fc269cc68f16a5faaa
m-yahya <yahya_gis@hotmail.com>
2019-10-31 13:43:00 +0100

    Format file

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,23 +1,23 @@
 <template>
   <div id="app">
-    <Main/>
+    <Main />
   </div>
 </template>
 
 <script>
-import Main from './components/Main.vue'
+import Main from "./components/Main.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     Main
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
